acfdc9ba16fa896446b7dc4958622b9308cdce4b
Zachary Honey <zhoney466@gmail.com>
2023-10-23 09:16:17 -0700

    wip

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 
-const server = require('./api/server')
-
-const port = process.env.port
+const server = require('./api/server.js')
+const port = process.env.PORT
 
 server.listen(port, () => console.log(`Server running on port ${port}`))
