0851115e9140b0bb6423719b9dade75651c85f51
Zack Honey <zhoney466@gmail.com>
2023-10-24 08:15:06 -0700

    made debugger

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,7 +4,7 @@ router.use('*', (req, res) => {
     res.json({ api: 'up' })
 })
 
-router.use((err, req, res, next) => {
+router.use((err, req, res, next) => {// eslint-disable-line
     res.status(500).json({
         customMessage: 'something went wrong inside the recipes-router',
         message: err.message,
