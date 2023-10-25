5a4773d27abe4ee081404d85d0e1f85af05ba731
Zachary Honey <zhoney466@gmail.com>
2023-10-25 09:33:22 -0700

    wip

--- knexfile.js
+++ knexfile.js
@@ -2,6 +2,7 @@ const sharedConfig = {
     client: 'sqlite3',
     migrations: { directory: './data/migrations' },
     pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+    useNullAsDefault: true,
 }
 
 module.exports = {
