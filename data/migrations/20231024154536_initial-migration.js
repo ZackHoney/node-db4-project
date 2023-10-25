44bc3ff4ae95f0825d5de6b39f65eecd76c9a369
Zachary Honey <zhoney466@gmail.com>
2023-10-25 09:10:39 -0700

    wip

--- data/migrations/20231024154536_initial-migration.js
+++ data/migrations/20231024154536_initial-migration.js
@@ -6,11 +6,11 @@ exports.up = async function (knex) {
     await knex.schema
         .createTable('recipes', table => {
            table.increments('recipe_id')
-           table.string('recipe_name', 200)
+           table.string('recipe_name', 200).notNullable().unique()
         })
         .createTable('ingredients', table => {
            table.increments('ingredient_id')
-           table.string('ingredient_name', 200)
+           table.string('ingredient_name', 200).notNullable().unique()
            table.string('ingredient_unit', 50)
         })
         .createTable('steps', table => {
