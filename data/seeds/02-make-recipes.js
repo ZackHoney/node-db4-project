d9c31672683c1ef49cc44438ca24fe45071241e7
Zachary Honey <zhoney466@gmail.com>
2023-10-25 10:05:15 -0700

    wip

--- data/seeds/02-make-recipes.js
+++ data/seeds/02-make-recipes.js
@@ -1,3 +1,12 @@
+const recipes = [
+    { recipe_name: 'Broccoli Pesto Pasta'},
+    { recipe_name: 'Lemon Chicken'},
+    { recipe_name: 'Salmon en Papillote'}
+]
+
+
+
+
 exports.seed = function (knex) {
 
 }
