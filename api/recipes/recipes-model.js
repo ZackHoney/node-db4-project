3d5482f56ac5edda23246451e6063c5addac4ba4
Zachary Honey <zhoney466@gmail.com>
2023-10-26 09:32:29 -0700

    wip

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -2,6 +2,13 @@ const db = require('../../data/db-config')
 
 async function getRecipeById(recipe_id) {
     const recipeRows = await db('recipes as r')
+    .leftJoin('steps as s', 'r.recipe_id', 's.recipe_id')
+    .select(
+        'r.recipe_id',
+
+    )
+    .where('r.recipe_id', recipe_id)
+     
 
     return recipeRows
 }
